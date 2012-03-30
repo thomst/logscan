c20c355b71d1feb4c25c5e88e8888ff319a99cda
Thomas Leichtfuss <thomaslfuss@gmx.de>
2012-03-30 15:10:54 +0200

    add url to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.2.1", 
     author = "Thomas Leichtfuss", 
     author_email = "thomaslfuss@gmx.de",
+    url = "https://github.com/thomst/logscan",
     download_url = "https://github.com/downloads/thomst/logscan/logscan-0.2.1.tar.gz",
     description = 'Command-line-tool to get time-specific access to log-files.',
     long_description = "This is a command-line-tool to get time-specific access to log-files, while rotated logfiles that are counted up (e.g. logfile.log, logfile.log.1, logfile.log.2.gz ...) are automatically taken together -also gzipped files are processed. Different date-time-formats are supported, inclusive simple timestamps as a huge number of seconds with three decimal-places, e.g. 123456789.123. Individual formats can be specified.",
@@ -28,5 +29,4 @@ setup(
     ],
     license='GPL',
     keywords='logfile logfiles log time-specific access rotated scan',
-    entry_points={'console_scripts': ['logscan = logscan:main', ]},
 )
